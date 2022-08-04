5f96d604664735f74bf780bdcb16e509a40fdbea
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-04 09:12:05 -0300

    Initial commit

---
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/hardhat.config.js
@@ -0,0 +1,11 @@
+require("@nomiclabs/hardhat-waffle");
+
+module.exports = {
+  solidity: "0.8.4",
+  networks: {
+    rinkeby: {
+      url: "https://rinkeby.infura.io/v3/<<PASTE YOUR INFURA PROJECT ID (DANGEROUS)>>",
+      accounts: ["<<PASTE YOUR PRIVATE KEY HERE (DANGEROUS)>>"],
+    },
+  },
+};
