d332473cb1f45277c60a793a1ba2d8538f99304b
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-04 10:51:29 -0300

    Enumerable

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
@@ -1,8 +1,27 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
+pragma solidity ^0.8.4;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
-contract PlatziPunks is ERC721 {
+// Override required
+contract PlatziPunks is ERC721, erc721Enumerable {
     constructor() ERC721("PlatziPunks", "PLPKS") {}
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint256 tokenId
+    ) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721Enumerable)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
 }
