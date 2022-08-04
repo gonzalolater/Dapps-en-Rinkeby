a01923ad225036a01eb1add2decbc4b4f540b039
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-04 09:34:15 -0300

    Primer Despliegue de contrato inteligente eninfura

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/hardhat.config.js
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/hardhat.config.js
@@ -4,8 +4,8 @@ module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/<<PASTE YOUR INFURA PROJECT ID (DANGEROUS)>>",
-      accounts: ["<<PASTE YOUR PRIVATE KEY HERE (DANGEROUS)>>"],
+      url: "https://rinkeby.infura.io/v3/<<PASTE YOUR INFURA PROJECT ID open (DANGEROUS)>>",
+      accounts: ["<<PASTE YOUR PRIVATE KEY HERE open (DANGEROUS)>>"],
     },
   },
 };
