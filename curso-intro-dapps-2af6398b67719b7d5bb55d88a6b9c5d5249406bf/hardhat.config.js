2e0ec89820163cefe4b2f1bdd8504397dd77a5e8
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-04 09:49:25 -0300

    seguridad de .env

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/hardhat.config.js
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/hardhat.config.js
@@ -1,11 +1,16 @@
 require("@nomiclabs/hardhat-waffle");
+requiere("dotenv").config();
+
+const projectId = process.env.INFURA_PROJECT_ID;
+const privateKey = process.env.EPLOYER_SGINER_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     rinkeby: {
-      url: "https://rinkeby.infura.io/v3/<<PASTE YOUR INFURA PROJECT ID open (DANGEROUS)>>",
-      accounts: ["<<PASTE YOUR PRIVATE KEY HERE open (DANGEROUS)>>"],
+      url: `https://rinkeby.infura.io/v3/${projectId} <<PASTE YOUR INFURA PROJECT ID open (DANGEROUS)>>`,
+      accounts: [privateKey],
+      //  ["<<PASTE YOUR PRIVATE KEY HERE  (DANGEROUS)>>"]
     },
   },
 };
