7857d378d26357b602e402678f0d93405d08bf1b
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-04 10:28:52 -0300

    deploy

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/scripts/deploy.js
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/scripts/deploy.js
@@ -1,5 +1,17 @@
-const deploy = async () => {};
+const deploy = async () => {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying contract with the account:", deployer.address);
+
+  const PlatziPunks = await ethers.getContractFactory("PlatziPunks");
+  const deployed = await PlatziPunks.deploy();
+
+  console.log("Platzi Punks is deployec at:", deployed.address);
+};
 
 deploy()
-  .them(() => process.exitCode(0))
-  .catch((error) => console.log(error));
+  .them(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
