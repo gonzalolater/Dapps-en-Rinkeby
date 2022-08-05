caed888ed59d994cccbce5c6f18d7c699544b7cf
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-05 08:22:19 -0300

    TokenUri, Json, Base64

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
+import "./Base64.sol";
 
 contract PlatziPunks is ERC721, ERC721Enumerable {
     using Counters for Counters.Counter;
@@ -21,6 +22,30 @@ contract PlatziPunks is ERC721, ERC721Enumerable {
 
         _safeMint(msg.sender, current);
     }
+
+    function tokenURI(uint256 tokenId) 
+        public 
+        view 
+        override 
+        returns (string memory ) 
+        {
+            requiere( 
+                _exist(tokenId) , 
+                "ERC721 Metadata: URI query for nonexistent token"
+                );
+
+            string memory jsonURI = Base64.encode(
+                abi.encodePacked(
+                    '{ "name": "PlatziPunks #',
+                    tokenId,
+                     '"", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
+                    "// TODO: Calculate image URL",
+                    '"}'
+                    )
+            );
+
+            return string(abi.encodePacked("data:application/json;base64,", jsonURI));
+        }
 
     // Override required
     function _beforeTokenTransfer(
