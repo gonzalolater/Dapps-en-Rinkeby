8f33a2ab5d6bac96c0ec95ab6b5197d85913ae7e
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-05 09:33:41 -0300

    Usando el ADN para calcular la imagen del NFT

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
@@ -12,6 +12,7 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
 
     Counters.Counter private _idCounter;
     uint256 public maxSupply;
+    mapping(uint256 => uint256) public tokenDNA;
 
     constructor(uint256 _maxSupply) ERC721("PlatziPunks", "PLPKS") {
         maxSupply = _maxSupply;
@@ -21,7 +22,59 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
         uint256 current = _idCounter.current();
         require(current < maxSupply, "No PlatziPunks left :(");
 
+        tokenDNA[current] = deterministicPseudoRandomDNA(current, msg.sender);
         _safeMint(msg.sender, current);
+    }
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://avataaars.io/";
+    }
+
+    function _paramsURI(uint256 _dna) internal view returns (string memory) {
+        string memory params;
+
+        {
+            params = string(
+                abi.encodePacked(
+                    "accessoriesType=",
+                    getAccessoriesType(_dna),
+                    "&clotheColor=",
+                    getClotheColor(_dna),
+                    "&clotheType=",
+                    getClotheType(_dna),
+                    "&eyeType=",
+                    getEyeType(_dna),
+                    "&eyebrowType=",
+                    getEyeBrowType(_dna),
+                    "&facialHairColor=",
+                    getFacialHairColor(_dna),
+                    "&facialHairType=",
+                    getFacialHairType(_dna),
+                    "&hairColor=",
+                    getHairColor(_dna),
+                    "&hatColor=",
+                    getHatColor(_dna),
+                    "&graphicType=",
+                    getGraphicType(_dna),
+                    "&mouthType=",
+                    getMouthType(_dna),
+                    "&skinColor=",
+                    getSkinColor(_dna),
+                    "&topType"
+                )
+            );
+
+            return params;
+        }
+
+        return string(abi.encodePacked(params, "&topType=", getTopType(_dna)));
+    }
+
+    function imageByDNA(uint256 _dna) public view returns (string memory) {
+        string memory baseURI = _baseURI();
+        string memory paramsURI = _paramsURI(_dna);
+
+        return string(abi.encodePacked(baseURI, "?", paramsURI));
     }
 
     function tokenURI(uint256 tokenId)
@@ -40,7 +93,7 @@ contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
                 '{ "name": "PlatziPunks #',
                 tokenId,
                 '"", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
-                "// TODO: Calculate image URL",
+                image,
                 '"}'
             )
         );
--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunksDNA.sol
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunksDNA.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.7;
+pragma solidity ^0.8.0;
 
 contract PlatziPunksDNA {
     string[] private _accessoriesType = [
@@ -229,7 +229,7 @@ contract PlatziPunksDNA {
             );
     }
 
-    function _getAccesoriesType(uint8 _dna)
+    function getAccessoriesType(uint256 _dna)
         public
         view
         returns (string memory)
@@ -238,7 +238,7 @@ contract PlatziPunksDNA {
         return _accessoriesType[dnaSection % _accessoriesType.length];
     }
 
-    function _getClotheColor(uint8 _dna) public view returns (string memory) {
+    function getClotheColor(uint256 _dna) public view returns (string memory) {
         uint8 dnaSection = _getDNASection(_dna, 2);
         return _clotheColor[dnaSection % _clotheColor.length];
     }
