c6652ea86870c0be6f11c5b5691bd5bdaac2fbb1
Gonzalo Daniel Aguilar <aguilar.daniel.gonzalo@gmail.com>
2022-08-05 08:49:45 -0300

    Configuracion de MetaData ADN PlatziPunks

--- curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
+++ curso-intro-dapps-2af6398b67719b7d5bb55d88a6b9c5d5249406bf/contracts/PlatziPunks.sol
@@ -5,8 +5,9 @@ import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "./Base64.sol";
+import "./PlatziPunksDNA.sol";
 
-contract PlatziPunks is ERC721, ERC721Enumerable {
+contract PlatziPunks is ERC721, ERC721Enumerable, PlatziPunksDNA {
     using Counters for Counters.Counter;
 
     Counters.Counter private _idCounter;
@@ -23,29 +24,30 @@ contract PlatziPunks is ERC721, ERC721Enumerable {
         _safeMint(msg.sender, current);
     }
 
-    function tokenURI(uint256 tokenId) 
-        public 
-        view 
-        override 
-        returns (string memory ) 
-        {
-            requiere( 
-                _exist(tokenId) , 
-                "ERC721 Metadata: URI query for nonexistent token"
-                );
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        requiere(
+            _exist(tokenId),
+            "ERC721 Metadata: URI query for nonexistent token"
+        );
 
-            string memory jsonURI = Base64.encode(
-                abi.encodePacked(
-                    '{ "name": "PlatziPunks #',
-                    tokenId,
-                     '"", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
-                    "// TODO: Calculate image URL",
-                    '"}'
-                    )
-            );
+        string memory jsonURI = Base64.encode(
+            abi.encodePacked(
+                '{ "name": "PlatziPunks #',
+                tokenId,
+                '"", "description": "Platzi Punks are randomized Avataaars stored on chain to teach DApp development on Platzi", "image": "',
+                "// TODO: Calculate image URL",
+                '"}'
+            )
+        );
 
-            return string(abi.encodePacked("data:application/json;base64,", jsonURI));
-        }
+        return
+            string(abi.encodePacked("data:application/json;base64,", jsonURI));
+    }
 
     // Override required
     function _beforeTokenTransfer(
